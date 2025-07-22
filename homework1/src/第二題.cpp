cdc21ea96c9e619899c37767bed5d46ae08b5195
51115122 <inall20070112@gmail.com>
2025-07-22 14:48:00 +0800

    Update 第二題.cpp

--- homework1/src/第二題.cpp
+++ homework1/src/第二題.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+void powerset(vector<char>& S, vector<vector<char>>& result, vector<char> current, int index) {
+    if (index == S.size()) {
+        result.push_back(current);
+        return;
+    }
+    powerset(S, result, current, index + 1);
+    current.push_back(S[index]);
+    powerset(S, result, current, index + 1);
+}
+
+vector<vector<char>> powerset(vector<char> S) {
+    vector<vector<char>> result;
+    vector<char> current;
+    powerset(S, result, current, 0);
+    return result;
+}
+
+int main() {
+    vector<char> S = {'a', 'b', 'c'};
+    vector<vector<char>> result = powerset(S);
+    for (const auto& subset : result) {
+        cout << "{ ";
+        for (char c : subset) {
+            cout << c << " ";
+        }
+        cout << "}" << endl;
+    }
+    return 0;
+}
